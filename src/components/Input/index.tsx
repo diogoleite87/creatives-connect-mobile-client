722e7b98f84e6aedcf2ca6542a76b7fdef2bbbd7
Diogo Leite <diogoleite87@gmail.com>
2023-02-28 21:15:15 -0300

    feat: add input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement } from "react";
+
+import { Container, TextInput } from './styles'
+
+interface propsInput {
+    icon?: ReactElement
+    value?: string
+    disable?: boolean
+    placeholder: string
+    secureTextEntry?: boolean
+    inputRef?: any
+    returnKeyType?: 'done' | 'go' | 'next' | 'search' | 'send'
+    keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad' | 'number-pad' | 'decimal-pad'
+    onChangeText: (text: string) => void
+    onSubmitEditing?: () => void
+
+}
+
+const Input: React.FC<propsInput> = ({
+    icon,
+    disable,
+    value,
+    placeholder,
+    inputRef,
+    secureTextEntry,
+    returnKeyType,
+    keyboardType,
+    onChangeText,
+    onSubmitEditing,
+}) => {
+    return (
+        <Container>
+            {icon && icon}
+            <TextInput
+                ref={inputRef}
+                placeholder={placeholder}
+                secureTextEntry={secureTextEntry}
+                onSubmitEditing={onSubmitEditing}
+                returnKeyType={returnKeyType}
+                keyboardType={keyboardType}
+                onChangeText={onChangeText}
+                autoCorrect={false}
+                autoCapitalize='none'
+                editable={disable}
+                defaultValue={value}
+            />
+        </Container>
+    )
+}
+
+export { Input }
