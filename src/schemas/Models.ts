6a2cba6309e0f63ee41160009f167f2156f7f6c2
Diogo Leite <diogoleite87@gmail.com>
2023-03-02 18:39:53 -0300

    feat: add schemas types

--- src/schemas/Models.ts
+++ src/schemas/Models.ts
@@ -8,3 +8,35 @@ export type AuthData = {
     },
     token: string
 }
+
+export type Profile = {
+    id: number
+    userName: string
+    name: string
+    email: string
+    profileImage: string
+    bio: string
+    city: string
+    birthday: string
+}
+
+export type Connect = {
+    id: number
+    name: string
+    userName: string
+    connectText: string
+    date: string
+    connectImage: string | null
+    liked: number
+    profileImage: string
+    comment: number
+}
+
+export type ConnectComment = {
+    id: number
+    name: string
+    userName: string
+    connectText: string
+    date: string
+    profileImage: string
+}
