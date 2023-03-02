040f88cba3b381049adf930ddf1d6e69d5c9cb51
Diogo Leite <diogoleite87@gmail.com>
2023-03-02 20:57:36 -0300

    update: aplication in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading'
+import { ThemeProvider } from 'styled-components/native'
 
-export default function App() {
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+} from '@expo-google-fonts/poppins'
+
+import COLORS from './src/styles/theme'
+
+import { AuthProvider } from './src/contexts/Auth';
+import { Router } from './src/routes/Router';
+
+const App: React.FC = () => {
+
+  const fontsLoaded = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ThemeProvider theme={COLORS}>
+
+      <StatusBar
+        style='dark'
+        translucent
+        backgroundColor='transparent'
+      />
+      <AuthProvider><Router /></AuthProvider>
+    </ThemeProvider>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -2,8 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from '../screens/Home';
 import { Profile } from '../screens/Profile';
 import { ConnectPage } from '../screens/ConnectPage';
-import { Connect } from '../schemas/Models';
-
+import { Settings } from '../screens/Settings';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +13,7 @@ export function AppStack() {
             <Stack.Screen name='Home' component={Home} />
             <Stack.Screen name='Profile' component={Profile} />
             <Stack.Screen name='ConnectPage' component={ConnectPage} />
+            <Stack.Screen name='Settings' component={Settings} />
         </Stack.Navigator>
     )
 }
