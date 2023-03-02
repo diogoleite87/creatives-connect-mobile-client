fd20092b7087e83531ff64650715417b32a039fa
Diogo Leite <diogoleite87@gmail.com>
2023-03-02 18:39:19 -0300

    feat: add connect screen

---
+++ src/screens/ConnectPage/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react"
+import { Container, ContainerHeader, ContainerProfileFooter, ContainerConnectHeader, ContainerProfileName, ProfileBio, ProfileImg, ProfileName, ProfileUser, ContainerAwesomeIcon, ConnectText, TextFooter, ContainerComment, ConnectImage, TextDate } from "./styles"
+import { ButtonBack } from "../../components/ButtonBack"
+import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart'
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons/faHeart'
+import { faComment } from '@fortawesome/free-regular-svg-icons/faComment'
+import { RFValue } from "react-native-responsive-fontsize"
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
+import { useNavigation } from "@react-navigation/native"
+import { CommentList } from "../../components/CommentList"
+import { Connect as ConnectType } from "../../schemas/Models"
+
+interface propsConnect {
+    route?: {
+        params: ConnectType
+    }
+}
+
+const ConnectPage: React.FC<propsConnect> = ({ route }) => {
+
+    const navigation = useNavigation();
+
+    useState(() => {
+        console.log(route?.params)
+    })
+
+    const [hasLiked, setHasLiked] = useState<boolean>(false)
+
+    return (
+        <Container>
+            <ContainerHeader>
+                <ButtonBack />
+                <ContainerConnectHeader onPress={() => navigation.navigate('Profile' as never)}>
+                    <ProfileImg source={{ uri: route?.params.profileImage }} />
+                    <ContainerProfileName>
+                        <ProfileName>{route?.params.name}</ProfileName>
+                        <ProfileUser>@{route?.params.userName}</ProfileUser>
+                    </ContainerProfileName>
+                </ContainerConnectHeader>
+                <ConnectText>{route?.params.connectText}</ConnectText>
+                {route?.params.connectImage ? <ConnectImage source={{ uri: route?.params.connectImage }} /> : <></>}
+                <TextDate>{route?.params.date}</TextDate>
+                <ContainerProfileFooter>
+                    <ContainerAwesomeIcon>
+                        <FontAwesomeIcon icon={faComment} size={RFValue(18)} />
+                        <TextFooter>0 comentários</TextFooter>
+                    </ContainerAwesomeIcon>
+                    <ContainerAwesomeIcon onPress={() => setHasLiked(!hasLiked)}>
+
+                        {hasLiked ? <FontAwesomeIcon icon={faHeartSolid} size={RFValue(18)} color="red" /> : <FontAwesomeIcon icon={faHeart} size={RFValue(18)} color="black" />}
+                        <TextFooter>{route?.params.liked} curtidas</TextFooter>
+                    </ContainerAwesomeIcon>
+                </ContainerProfileFooter>
+            </ContainerHeader>
+            <ContainerComment>
+                <CommentList />
+            </ContainerComment>
+        </Container>
+    )
+}
+
+export { ConnectPage }
---
+++ src/screens/ConnectPage/styles.ts
@@ -0,0 +1,100 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+  padding: ${RFValue(16)}px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUNDLIGHT};
+`
+
+export const ContainerComment = styled.View`
+  margin-top: ${RFValue(16)}px;
+  width: 100%;
+`
+
+export const ContainerHeader = styled.View`
+  margin-top: ${RFValue(getStatusBarHeight(true))}px;
+  width: 100%;
+`
+
+export const ContainerProfile = styled.View`
+  margin-top: ${RFValue(16)}px;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+`
+
+export const ContainerConnectHeader = styled.TouchableOpacity`
+  margin-top: ${RFValue(16)}px;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const ProfileImg = styled.Image`
+  margin: 10px;
+  width: 70px;
+  height: 70px;
+  border-radius: 100px;
+`
+
+export const ProfileUser = styled.Text`
+  font-size: ${RFValue(14)}px;
+`
+
+export const ContainerProfileName = styled.View` 
+  margin: 5px;
+  flex-direction: column;
+`
+export const ProfileName = styled.Text`
+  font-size: ${RFValue(18)}px;
+  color: black;
+  font-weight: bold;
+`
+
+export const ProfileBio = styled.Text`
+  margin: 5px;
+  font-size: ${RFValue(12)}px;
+  color: black;
+`
+
+export const ContainerProfileFooter = styled.View`
+  margin: 5px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ContainerAwesomeIcon = styled.TouchableOpacity`
+   align-items: center;
+   flex-direction: row;
+`
+
+export const ConnectText = styled.Text`
+  margin: 5px;
+  font-size: ${RFValue(14)}px;
+  color: black;
+`
+
+export const TextFooter = styled.Text`
+  font-size: ${RFValue(12)}px;
+  margin-left: 5px;
+`
+
+export const ConnectImage = styled.Image`
+  margin: 5px;
+  width: 100%;
+  height: 350px;
+  border-radius: 10px;
+`
+
+export const TextDate = styled.Text`
+  margin: 5px;
+  font-size: ${RFValue(12)}px;
+  color: ${({ theme }) => theme.COLORS.BLUE1};
+`
