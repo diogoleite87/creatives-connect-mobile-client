ec14d520d75ec92ea3ce087d99d4f3c4617dbd42
Diogo Leite <diogoleite87@gmail.com>
2023-03-04 10:45:15 -0300

    feat: add input image button

---
+++ src/components/InputImage/index.tsx
@@ -0,0 +1,36 @@
+import { Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { ButtonText, Container } from './styles';
+import { faPen } from '@fortawesome/free-solid-svg-icons/faPen';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { ButtonEdit } from '../ButtonEdit';
+
+interface propsInputImage {
+    setImage(image: string): void,
+}
+
+const InputImage: React.FC<propsInputImage> = ({ setImage }) => {
+
+    const pickImage = async () => {
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
+    return (
+        <ButtonEdit onPress={pickImage} />
+    );
+};
+
+export { InputImage }
