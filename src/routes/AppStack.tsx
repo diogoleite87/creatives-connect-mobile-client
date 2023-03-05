caf17fe27f6b1b3c19ce999902fa755a79726fc3
Diogo Leite <diogoleite87@gmail.com>
2023-03-05 12:00:50 -0300

    feat: add new connect screen

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -3,6 +3,8 @@ import { Home } from '../screens/Home';
 import { Profile } from '../screens/Profile';
 import { ConnectPage } from '../screens/ConnectPage';
 import { Settings } from '../screens/Settings';
+import { NewCommentPage } from '../screens/NewCommentPage';
+import { NewConnectPage } from '../screens/NewConnectPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,8 @@ export function AppStack() {
             <Stack.Screen name='Profile' component={Profile} />
             <Stack.Screen name='ConnectPage' component={ConnectPage} />
             <Stack.Screen name='Settings' component={Settings} />
+            <Stack.Screen name='NewCommentPage' component={NewCommentPage} />
+            <Stack.Screen name='NewConnectPage' component={NewConnectPage} />
         </Stack.Navigator>
     )
 }
