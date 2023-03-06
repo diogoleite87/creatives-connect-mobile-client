8a36c126b9e57a9800ee7d3ed5d9962c23bdb81e
Filipe <filipeasm18@gmail.com>
2023-03-06 09:39:49 -0300

    Fix: Traceback Home component

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,5 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { ConnectPage } from "../screens/ConnectPage"
+import { Home } from "../screens/Home"
+import { NewCommentPage } from "../screens/NewCommentPage"
+import { NewConnectPage } from "../screens/NewConnectPage"
 import { Profile } from "../screens/Profile"
 import { Settings } from "../screens/Settings"
 
@@ -11,7 +14,7 @@ export function AppStack() {
       screenOptions={{ headerShown: false }}
       initialRouteName="Home"
     >
-      <Stack.Screen name="Home" component={Showcase} />
+      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="ConnectPage" component={ConnectPage} />
       <Stack.Screen name="Settings" component={Settings} />
