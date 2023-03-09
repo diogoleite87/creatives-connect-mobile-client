c16ae794173651c783823e9fc7a517bbf7a78f94
Diogo Leite <diogoleite87@gmail.com>
2023-03-08 22:15:06 -0300

    feat: api function follow and unfollow integration

--- src/components/ButtonFollow/index.tsx
+++ src/components/ButtonFollow/index.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 
 import { ButtonText, ContainerFollow, ContainerUnfollow } from "./styles";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import { FOLLOW_USER } from "../../graphql/user/mutations";
+import { MutationFollowUserVariables, MutationFollowUser } from "../../generated/api-types";
+import { useAuth } from "../../hooks/useAuth";
 
 interface propsButtonFollow {
     userFollow: string
@@ -9,24 +13,53 @@ interface propsButtonFollow {
 const ButtonFollow: React.FC<propsButtonFollow> = ({ userFollow }) => {
 
     const [isFollowing, setIsFollowing] = useState<boolean>(false)
+    const { authData } = useAuth()
 
-    useEffect(() => {
-
-    }, [])
+    const submit = () => {
+        isFollowing ? unFollow() : follow()
+    }
 
     const follow = async () => {
-        setIsFollowing(!isFollowing)
+        console.log('seguindo')
+        await followUser({
+            variables: {
+                sourceUsername: authData?.userName!,
+                sinkUsername: userFollow
+            }, onCompleted(data, clientOptions) {
+                setIsFollowing(!isFollowing)
+            },
+        })
     }
 
     const unFollow = async () => {
-        setIsFollowing(!isFollowing)
+        console.log('deixando de seguir')
+        await unfollowUser({
+            variables: {
+                sourceUsername: authData?.userName!,
+                sinkUsername: userFollow
+            }, onCompleted(data, clientOptions) {
+                setIsFollowing(!isFollowing)
+            },
+        })
     }
+
+    const [followUser, { loading: followUserLoading, error: followUserError }] = useMutation(gql`
+    mutation followUser($sourceUsername: String!, $sinkUsername: String!) {
+        followUser(sourceUsername: $sourceUsername, sinkUsername: $sinkUsername)
+      }
+    `);
+
+    const [unfollowUser, { loading: unFollowUserLoading, error: unFollowUserError }] = useMutation(gql`
+    mutation followUser($sourceUsername: String!, $sinkUsername: String!) {
+        unfollowUser(sourceUsername: $sourceUsername, sinkUsername: $sinkUsername)
+      }
+    `);
 
     return (
         isFollowing ? <ContainerUnfollow onPress={unFollow}>
             <ButtonText>Seguindo</ButtonText>
         </ContainerUnfollow> :
-            <ContainerFollow onPress={follow}>
+            <ContainerFollow onPress={submit}>
                 <ButtonText>Seguir</ButtonText>
             </ContainerFollow>
     )
