c6bbcad713a39f250065997b0168ec282a74e354
Diogo Leite <diogoleite87@gmail.com>
2023-03-10 16:24:52 -0300

    fix: date return string

--- src/utils/timestampToDate.ts
+++ src/utils/timestampToDate.ts
@@ -1,8 +1,9 @@
-export function timestampToDate(timestamp: number): string {
-    const date = new Date(timestamp * 1000);
+export function timestampToDate(seconds: number) {
+    const date = new Date(seconds);
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    const month =
+        date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
 
     return `${day}/${month}/${year}`;
 }
